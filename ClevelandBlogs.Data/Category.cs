126a7ddc137db83c3a4b5fecd28e5ac33e3738f1
Tom <tomrambo95@yahoo.com>
2020-11-18 16:31:51 -0500

    Finished Setup

---
+++ ClevelandBlogs.Data/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClevelandBlogs.Data
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        [Display(Name = "Category")]
+        public string Title { get; set; }
+    }
+}
